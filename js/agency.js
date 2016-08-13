343e8c9115cb5f280ed5d5bf5702b9e43933d7e0
Nhu Luong <nhuluong2212@gmail.com>
2016-08-12 23:47:51 -0700

    08/12

--- js/agency.js
+++ js/agency.js
@@ -13,11 +13,12 @@
         event.preventDefault();
     });
 
-    // // Highlight the top nav as scrolling occurs
-    // $('body').scrollspy({
-    //     target: '.navbar-fixed-top',
-    //     offset: 51
-    // });
+    // Highlight the top nav as scrolling occurs
+    $('body').scrollspy({
+        target: '#mainNav', 
+        offset: 100 ,
+
+    });
 
     // Closes the Responsive Menu on Menu Item Click
     $('.navbar-collapse ul li a:not(.dropdown-toggle)').click(function() {
@@ -25,12 +26,44 @@
         
     });
 
-    // Offset for Main Navigation
+    //Offset for Main Navigation
     $('#mainNav').affix({
         offset: {
-            top: 80
+            top: 100    
         }
     })
+     $(window).scroll(function() {
+                var windowsHeight = $(document).height() - $(window).height();
+                var currentScroll = $(window).scrollTop();
+                
+                var top = $('#mainNav').height();
+                var aboutustop = $('#aboutus').position().top;
+                var membershiptop = $('#membership').position().top;
+                var membershipbottom = membershiptop + $(membership).height();
+
+
+                console.log(currentScroll);
+                 if( currentScroll > aboutustop - 70 && currentScroll < membershiptop - 20){
+                    $(".navbar-custom").css("background-color","#5C5379");
+                    console.log('our of range');
+                } 
+                else if (currentScroll > membershiptop - 20 && currentScroll < membershipbottom + 200){
+                    $(".navbar-custom").css("background-color","rgba(255 , 255, 255, 0.2)");
+                     $(".navbar-collapse ").css("background-color","");
+                     console.log('in range');
+                }
+                else if (currentScroll > membershipbottom + 100){
+                    $(".navbar-custom").css("background-color","#5C5379");
+                    console.log('after range');
+                }      
+                else {
+                     $(".navbar-custom").css("background-color","rgba(255 , 255, 255, 0.2)");
+                     $(".navbar-collapse ").css("background-color","");
+                        
+            }
+     });
+   
+            
 
 
 })(jQuery); // End of use strict
